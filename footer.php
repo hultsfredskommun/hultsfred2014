eeb05e19adfafa1e897ff1d5b56d36bd569ad171
Jonas Hjalmarsson <jonas.hjalmarsson@hultsfred.se>
2014-10-03 15:35:44 +0200

    changes
    Signed-off-by: jonas <jonas@jonas-HP-EliteBook-Folio-9470m>

--- footer.php
+++ footer.php
@@ -16,13 +16,14 @@
 
 			<?php get_sidebar( 'footer' ); ?>
 
+			<div class="panorama-wrapper">
+				<img src="http://www.hultsfred.se/files/2013/03/panorama_webb.png" alt="Hultsfreds kommun" />
+		     	</div>
 			<div class="site-info">
 				<?php do_action( 'twentyfourteen_credits' ); ?>
 				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentyfourteen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyfourteen' ), 'WordPress' ); ?></a>
 			</div><!-- .site-info -->
-		<div class="panorama-wrapper">
-		     <img src="http://www.hultsfred.se/files/2013/03/panorama_webb.png" alt="Hultsfreds kommun" />
-		</div>
+
 		</footer><!-- #colophon -->
 	</div><!-- #page -->
 
