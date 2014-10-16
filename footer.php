c8065dc8a1a3517e7bd50ab3868df1c46b5f0d49
administrator <administrator@hks-webb-test.hultsfred.se>
2014-10-16 11:38:41 +0200

    login added

--- footer.php
+++ footer.php
@@ -21,7 +21,7 @@
 		     	</div>
 			<div class="site-info">
 				<?php do_action( 'twentyfourteen_credits' ); ?>
-				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentyfourteen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyfourteen' ), 'WordPress' ); ?></a>
+				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentyfourteen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyfourteen' ), 'WordPress' ); ?></a> | <?php wp_loginout(); ?>
 			</div><!-- .site-info -->
 
 		</footer><!-- #colophon -->
