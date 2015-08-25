3bfd959262deb2e47cc3a7f69c2564672dbd0923
administrator <administrator@hks-webb-007.hks-webb-007.hultsfred.se>
2015-08-25 07:19:42 +0200

    menu responsive bug fix

--- header.php
+++ header.php
@@ -29,69 +29,41 @@
 	<![endif]-->
 	<?php wp_head(); ?>
 </head>
-<?php $hide_secondary = get_post_meta( get_the_ID(), 'hide-secondary', true ); 
-$hide_secondary_class = "";
-if ($hide_secondary == "true") $hide_secondary_class = "hide-secondary";
-?>
-<body <?php body_class($hide_secondary_class); ?>>
+
+<body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
 	<?php if ( get_header_image() ) : ?>
 	<div id="site-header">
 		<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-			<img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="">
+			<img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">
 		</a>
 	</div>
 	<?php endif; ?>
-	
-	<header id="masthead" class="site-header" role="banner">
 
+	<header id="masthead" class="site-header" role="banner">
 		<div class="header-main">
 			
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<h1 class="site-title"><a class="logo_h1" href="http://www.hultsfred.se/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/Hultsfreds_kommun_vapen.png" alt="Hultsfreds kommun"></a><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 
-			<div class="logo">
-				<span id="logo" class="logoimg"><a href="http://www.hultsfred.se/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/Hultsfreds_kommun_blO_grO_blOtxt.png" alt="Hultsfreds kommun"></a></span>
-			</div>
 			<div class="search-toggle">
-				<a href="#search-container" class="screen-reader-text"><?php _e( 'Search', 'twentyfourteen' ); ?></a>
-		
+				<a href="#search-container" class="screen-reader-text" aria-expanded="false" aria-controls="search-container"><?php _e( 'Search', 'twentyfourteen' ); ?></a>
 			</div>
-			
-			<button class="menu-toggle"><?php _e( 'Primary Menu', 'twentyfourteen' ); ?></button>
-			
-			<?php if (get_current_blog_id() > 1) : ?>
-			<nav id="primary-sticky-navigation" class="site-navigation primary-navigation primary-sticky-navigation" role="navigation">
-				<div class="nav-menu">
-					<ul>
+
+			<nav id="primary-navigation" class="site-navigation primary-navigation" role="navigation">
+				<button class="menu-toggle"><?php _e( 'Primary Menu', 'twentyfourteen' ); ?></button>
+				<a class="screen-reader-text skip-link" href="#content"><?php _e( 'Skip to content', 'twentyfourteen' ); ?></a>
+				<ul id="primary-menu" class="nav-menu" >
+				<?php wp_nav_menu( array( 'items_wrap' => '%3$s', 'container' => false, 'theme_location' => 'primary', 'menu_class' => '', 'menu_id' => '' ) ); ?>
+				<?php if (get_current_blog_id() > 1) : ?>
+					
 						
 						<li class="page_item"><a href="/">Alla bloggar</a>
-							<?php //, 'container' => '', 'items_wrap' => ''
-							/*
-							<ul class="children">
-							$blog_list = get_blog_list( 0, 'all' ); 
-							foreach ($blog_list AS $blog) { 
-								if ($blog['blog_id'] != "1") { 
-									$details = get_blog_details($blog['blog_id']); 
-									echo '<li class="page_item"><a href="' . $details->siteurl . '">' . $details->blogname . '</a>';
-									echo '</li>';
-								}
-							}
-							</ul>*/
-							?>
-							
 						</li>
 						<!--li class="page_item"><a href="http://www.hultsfred.se/">Till hultsfred.se</a></li-->
 					</ul>
-				</div>
-			</nav>
-			<?php endif; ?>
-
-			<nav id="primary-navigation" class="site-navigation primary-navigation" role="navigation">
 				
-				<a class="screen-reader-text skip-link" href="#content"><?php _e( 'Skip to content', 'twentyfourteen' ); ?></a>
-				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
+				<?php endif; ?>
 			</nav>
-
 		</div>
 
 		<div id="search-container" class="search-box-wrapper hide">
@@ -99,8 +71,6 @@ if ($hide_secondary == "true") $hide_secondary_class = "hide-secondary";
 				<?php get_search_form(); ?>
 			</div>
 		</div>
-
 	</header><!-- #masthead -->
 
 	<div id="main" class="site-main">
-
